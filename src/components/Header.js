5f0ce08302691c5dd8e2338174290402e291e6ed
Aaron Billings <asbillings2007@gmail.com>
2019-07-11 21:03:44 -0400

    edit layout and name

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
-import React from "react";
-import Nav from "./Nav";
-import SearchForm from "./SearchForm";
+import React from 'react';
+import Nav from './Nav';
+import SearchForm from './SearchForm';
 
 const Header = ({ performSearch, data, updateText }) => {
   return (
     <React.Fragment>
-      <h1>React Gallery App</h1>
+      <h1>Gallerize</h1>
+      <h3>
+        <em>A React Photo Gallery App</em>
+      </h3>
       <SearchForm onSearch={performSearch} data={data} />
       <Nav getThatText={updateText} search={performSearch} data={data} />
     </React.Fragment>
